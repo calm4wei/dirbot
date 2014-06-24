b0e0913a75bfe74bbabe515d25826ca0f265c216
nullspace <jxhchina@gmail.com>
2014-06-25 02:14:35 +0800

    fail 1

--- dirbot/settings.py
+++ dirbot/settings.py
@@ -3,5 +3,5 @@
 SPIDER_MODULES = ['dirbot.spiders']
 NEWSPIDER_MODULE = 'dirbot.spiders'
 DEFAULT_ITEM_CLASS = 'dirbot.items.Website'
-LOG_ENABLED = True
+LOG_ENABLED = False
 #ITEM_PIPELINES = {'dirbot.pipelines.FilterWordsPipeline': 1}
---
+++ dirbot/spiders/youyousuiyue1.py
@@ -0,0 +1,59 @@
+from scrapy.spider import Spider
+from scrapy.selector import Selector
+
+from dirbot.items import Article
+
+import json
+import re
+import string
+from scrapy.http import Request
+
+class YouyousuiyueSpider(Spider):
+    name = "youyousuiyue1"
+    allowed_domains = ["youyousuiyue.sinaapp.com"]
+    
+    start_urls = [
+        'http://youyousuiyue.sinaapp.com',
+    ]
+        
+    def parse_item(self, response):
+        """
+        The lines below is a spider contract. For more info see:
+        http://doc.scrapy.org/en/latest/topics/contracts.html
+
+        @url http://youyousuiyue.sinaapp.com
+        @scrapes name
+        """
+        sel = Selector(response)
+        items = []
+
+        articles = sel.xpath('//div[@id="content"]/article')
+        
+        for d in articles:
+            item = Article()
+            title = d.xpath('header/h1/a')
+            item['title'] = title.xpath('text()').extract()
+            print item['title'][0]
+            item['url'] = title.xpath('@href').extract()
+            item['content'] = d.xpath('div[@class="entry-content"]/div/text()').extract()
+            items.append(item)
+        return items
+
+    def parse(self, response):
+        """
+        The lines below is a spider contract. For more info see:
+        http://doc.scrapy.org/en/latest/topics/contracts.html
+
+        @url http://youyousuiyue.sinaapp.com
+        @scrapes name
+        """
+        sel = Selector(response)
+        items = []
+
+        link = sel.xpath('//div[@class="nav-previous"]/a/@href').extract()[0]
+        print 'link-->', link
+        if link[-1] == '4':
+            yield None
+        else:
+            print 'yielding...'
+            yield Request(link, callback=self.parse_item)
