106a755c6557229147ce22e297541c3eb130fc4d
nullspace <jxhchina@gmail.com>
2014-06-27 02:41:57 +0800

    count

--- iaf/iaf_en_last_name.py
+++ iaf/iaf_en_last_name.py
@@ -21,7 +21,11 @@ for p in persons:
         else:
             lnMap[ln] = [n]
 
-        print ln, len(lnMap[ln])
+fout = file('en_last_name_count.txt', 'w')
+for ln in lnMap.keys():
+	s = '%s\t%d' %(ln, len(lnMap[ln]))
+	print s
+	fout.write(s + '\n')
 
 json.dump(lnMap, file('iaf_en_last_names.json', 'w'))
 print len(lnMap)
