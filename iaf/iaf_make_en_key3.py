2bc6c4cb5dae906438f3852fabc4fc1e85b60dd7
nullspace <jxhchina@gmail.com>
2014-06-27 20:59:20 +0800

    key upper

--- iaf/iaf_make_en_key3.py
+++ iaf/iaf_make_en_key3.py
@@ -4,13 +4,13 @@
 import json
 import re
 
-f = file('iaf_en_names_clean1.json', 'r')
+f = file('iaf_map_clean1.json', 'r')
 personMap = json.load(f)
 
 personMap1 = {}
 for k in personMap.keys():
-    newk = k.upper();
-    personMap1[newk] = personMap[k].upper();
+    newk = k.upper()
+    personMap1[newk] = personMap[k]
 
-f = file('iaf_en_names_clean2.json', 'w')
+f = file('iaf_map_clean2.json', 'w')
 json.dump(personMap1, f)
---
+++ iaf/iaf_make_en_key4.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import json
+import re
+
+f = file('iaf_first_names.json', 'r')
+personMap = json.load(f)
+
+personMap1 = {}
+for k in personMap.keys():
+    newk = k.upper();
+    vs = personMap[k];
+    vs1 = []
+    for v in vs:
+        v = v.upper()
+        vs1.append(v)
+    personMap1[newk] = vs1
+
+f = file('iaf_first_names1.json', 'w')
+json.dump(personMap1, f)
